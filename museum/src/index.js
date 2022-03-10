45e6a5ffaeefe019517f2c982127ed86ecb094f4
evgeniiDJS <hronologia2019@gmail.com>
2022-03-10 12:44:23 +0400

    fix: gallery and tickets

--- museum/src/index.js
+++ museum/src/index.js
@@ -1,5 +1,4 @@
 import slider from './js/slider.js'; slider;
-import disablingScrolling from './js/disablingScrolling.js'; disablingScrolling;
 import cardInputControl from './js/cardInputControl.js'; cardInputControl;
 
 
